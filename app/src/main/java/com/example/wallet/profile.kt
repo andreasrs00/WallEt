bdff6d5093b1dc229bf201e9af167e79f203c51c
alvianrml <alvianrmli@gmail.com>
2024-11-10 18:37:32 +0700

    profil tanpa table

--- app/src/main/java/com/example/wallet/profile.kt
+++ app/src/main/java/com/example/wallet/profile.kt
@@ -1,10 +1,15 @@
 package com.example.wallet
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,16 @@ class profile : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val textView: TextView = findViewById(R.id.tvrecentview)
+        val text = getString(R.string.recent)
+        val spannable = SpannableString(text)
+
+
+        spannable.setSpan(UnderlineSpan(), text.indexOf("Transaction"), text.length, 2)
+
+        textView.text = spannable
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 2) // 2 kolom
     }
 }
